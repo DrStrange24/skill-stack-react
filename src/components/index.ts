7ffa3669cafad08475d016a694c9bfe3f48ff58a
anonymous <jbrynnbacuta@gmail.com>
2024-08-17 00:02:28 +0800

    add icon in package

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { Header } from "./Header";
 export { Table, type Column as TableColumn, type TableProps } from "./Table";
 export { Loading } from "./Loading";
+export { ConfirmationModal } from "./ConfirmationModal";
